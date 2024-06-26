27ec1d07475307aeb8d063e51246f53c407aa1da
Santiago Mendoza <santiagoyasno@gmail.com>
2024-06-26 16:41:05 -0500

    Fix dark mode (Needs improve)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
-document.getElementById("theme-toggle").addEventListener("click", function () {
-  document.body.classList.toggle("dark-theme");
+document.addEventListener("DOMContentLoaded", () => {
+  const darkMode = localStorage.getItem("darkMode");
+
+  if (darkMode === "enabled") {
+    document.body.classList.add("dark-theme");
+  }
+
+  const themeToggle = document.getElementById("theme-toggle");
+
+  themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+
+    // Save preference to localStorage
+    if (document.body.classList.contains("dark-theme")) {
+      localStorage.setItem("darkMode", "enabled");
+    } else {
+      localStorage.setItem("darkMode", "disabled");
+    }
+  });
 });
